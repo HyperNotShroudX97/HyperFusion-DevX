6facd8dce37ccd4b287eabb4551c4d0d18efb38f
NotShroudX97 <skyebeatzx97@gmail.com>
2021-09-07 02:42:03 +0530

    alive.py

--- userge/plugins/bot/alive.py
+++ userge/plugins/bot/alive.py
@@ -257,8 +257,12 @@ class Bot_Alive:
             [
                 InlineKeyboardButton(text="✖️  HyperMTKGang!", url="t.me/HyperMTKGang"),
                 InlineKeyboardButton(text="⚡  REPO!", url=Config.UPSTREAM_REPO),
+            ],
+            [
                 InlineKeyboardButton(text="☣️  Updates!", url="t.me/UsergeXForkUpdates"),
-                InlineKeyboardButton(text="☢️  Support!", url="t.me/UsergeXForkTeam"),      ]
+                InlineKeyboardButton(text="☢️  Support!", url="t.me/UsergeXForkTeam"),
+            ],
+       ]
         return InlineKeyboardMarkup(buttons)
 
     @staticmethod
