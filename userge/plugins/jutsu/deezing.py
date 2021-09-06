6e476a54e5e1adc4ffe8ccf12b1f134f7db21134
NotShroudX97 <skyebeatzx97@gmail.com>
2021-09-06 11:57:21 +0530

    deezing.py

--- userge/plugins/jutsu/deezing.py
+++ userge/plugins/jutsu/deezing.py
@@ -12,13 +12,13 @@ from userge.utils import capitaled
 @userge.on_cmd(
     "dz",
     about={
-        "header": "Deezer Music",
-        "description": "Download Music From Deezer",
-        "usage": "{tr}dz [Artist Name] [Song Name] [; Number](Optional)",
+        "header": "deezer music",
+        "description": "download music from deezer",
+        "usage": "{tr}dz [artist name] [song name] [; number](optional)",
     },
 )
 async def deezing_(message: Message):
-    """Download Music From Deezer"""
+    """download music from deezer"""
     query_ = message.input_str
     if ";" in query_:
         split_ = query_.split(";", 1)
@@ -29,14 +29,14 @@ async def deezing_(message: Message):
     try:
         num = int(num)
     except BaseException:
-        await message.edit("Please Enter A Proper Number After ';'...", del_in=5)
+        await message.edit("Please enter a proper number after ';'...", del_in=5)
         return
-    bot_ = "DeezerMusicBot"
+    bot_ = "deezermusicbot"
     song_ = await capitaled(song_)
-    await message.edit(f"Searching <b>{song_}</b> On Deezer...")
+    await message.edit(f"Searching <b>{song_}</b> on deezer...")
     results = await userge.get_inline_bot_results(bot_, song_)
     if not results.results:
-        await message.edit(f"Song <code>{song_}</code> Not Found...", del_in=5)
+        await message.edit(f"Song <code>{song_}</code> not found...", del_in=5)
         return
     try:
         log_send = await userge.send_inline_bot_result(
@@ -54,32 +54,32 @@ async def deezing_(message: Message):
         )
     except BaseException:
         await message.err(
-            "Something Unexpected Happend, Please Try Again Later...", del_in=5
+            "Something unexpected happend, please try again later...", del_in=5
         )
 
 
 @userge.on_cmd(
     "dzlist",
     about={
-        "header": "Deezer Music List",
-        "description": "Get Music List From Deezer"
-        "\nSudo Users Use DZ After Getting The List",
-        "usage": "{tr}dzlist [Query]",
+        "header": "deezer music list",
+        "description": "get music list from deezer"
+        "\nSudo users use dz after getting the list",
+        "usage": "{tr}dzlist [query]",
     },
 )
 async def dlist_(message: Message):
-    """Get List And Number Corresponding To Song's"""
+    """get list and number corresponding to songs"""
     bot_ = "deezermusicbot"
     query_ = message.input_str
     if not query_:
-        await message.err("Input Not Found...", del_in=5)
+        await message.err("Input not found...", del_in=5)
         return
     query_ = await capitaled(query_)
-    await message.edit(f"Searching For <b>{query_}</b>...")
+    await message.edit(f"Searching for <b>{query_}</b>...")
     result = await userge.get_inline_bot_results(bot_, query_)
     if not result:
         await message.edit(
-            f"Results Not Found For <code>{query_}</code>, Try Something Else...",
+            f"Results not found for <code>{query_}</code>, try something else...",
             del_in=5,
         )
         return
@@ -99,54 +99,66 @@ async def dlist_(message: Message):
             break
     if not list_:
         await message.edit(
-            f"Couldn't Find Results For <code>{query_}</code>...", del_in=5
+            f"Couldn't find results for <code>{query_}</code>...", del_in=5
         )
         return
     list_ = "\n".join(list_)
-    out_ = f"Results Found For <b>{query_}</b>: [<b>{total_}</b>]\n\n"
+    out_ = f"Results found for <b>{query_}</b>: [<b>{total_}</b>]\n\n"
     out_ += list_
-    out_ += "\n\nReply With Corresponding Number <b>Within 20 Seconds </b> To Get The Music."
+    out_ += (
+        "\n\nReply with corresponding number <b>within 15 seconds</b> to get the music."
+    )
     await message.edit(out_)
     me_ = await userge.get_me()
+    reply_ = []
     try:
-        async with userge.conversation(message.chat.id, timeout=20) as conv:
+        async with userge.conversation(message.chat.id, timeout=15) as conv:
             response = await conv.get_response(
                 mark_read=True, filters=(filters.user(me_.id))
             )
             resp = response.text
+            resp = resp.split()
             try:
-                reply_ = int(resp)
+                for one in resp:
+                    int(one)
+                    reply_.append(one)
             except BaseException:
-                out_ += f"\n\n### The Response <b>{resp}</b> Is Not A Number, <b>Please Retry</b>. ###"
+                proverb = "is not" if len(resp) == 1 else "are not all"
+                resp = "</b>, <b>".join(resp)
+                out_ += f"\n\n### The response <b>{resp}</b> {proverb} a number, <b>please retry</b>. ###"
                 await response.delete()
                 await message.edit(out_, del_in=15)
                 return
             try:
-                result_id = result.results[reply_].id
+                for one in reply_:
+                    result_id = result.results[int(one)].id
             except BaseException:
-                out_ += f"\n\n### Response <b>{resp}</b> Gave Out Of Index Error, <b>Please Retry</b>. ###"
+                out_ += f"\n\n### Response/s <b>{reply_}</b> gave out of index error, <b>please retry</b>. ###"
                 await response.delete()
                 await message.edit(out_, del_in=15)
                 return
             await response.delete()
     except BaseException:
-        out_ += "\n\n### <b>Response Time Expired.</b> ###"
+        out_ += "\n\n### <b>Response time expired.</b> ###"
         await message.edit(out_)
         return
     try:
-        log_send = await userge.send_inline_bot_result(
-            chat_id=Config.LOG_CHANNEL_ID,
-            query_id=result.query_id,
-            result_id=result_id,
-        )
-        await userge.copy_message(
-            chat_id=message.chat.id,
-            from_chat_id=Config.LOG_CHANNEL_ID,
-            message_id=log_send.updates[0].id,
-        )
-        out_ += f"\n\n### <b>Responded With {reply_}.</b> ###"
+        for one in reply_:
+            result_id = result.results[int(one)].id
+            log_send = await userge.send_inline_bot_result(
+                chat_id=Config.LOG_CHANNEL_ID,
+                query_id=result.query_id,
+                result_id=result_id,
+            )
+            await userge.copy_message(
+                chat_id=message.chat.id,
+                from_chat_id=Config.LOG_CHANNEL_ID,
+                message_id=log_send.updates[0].id,
+            )
+        reply_ = ", ".join(reply_)
+        out_ += f"\n\n### <b>Responded with {reply_}.</b> ###"
         await message.edit(out_)
     except BaseException:
         await message.err(
-            "Something Unexpected Happend, Please Try Again Later...", del_in=5
+            "Something unexpected happend, please try again later...", del_in=5
         )
